1ded3cf617df2d7985dd8b93fb836a6ef628226b
StamatisTiniakos <st88@hw.ac.uk>
2018-01-30 12:18:48 +0000

    Basic classes

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,13 +2,7 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		//Stam's test comment
-		//George 239
-		// hey
-		// TODO Auto-generated method stub 
 		
-		// stam has the same meal every day for 8 years straight..
-
 	}
 
 }
