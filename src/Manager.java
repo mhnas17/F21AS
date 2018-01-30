34f2027b2a310fcdeedd0d870daa19b30f98cf1f
StamatisTiniakos <st88@hw.ac.uk>
2018-01-30 19:06:18 +0000

    stam

--- src/Manager.java
+++ src/Manager.java
@@ -3,6 +3,8 @@ public class Manager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Data s= new Data();
+		s.process("textfile1", "textfile2");
 		NewGui demo = new NewGui();
 	}
 
