adea7fb5d9069e2ec15f6b8890cb16a9b17c1aef
StamatisTiniakos <st88@hw.ac.uk>
2018-01-30 12:15:09 +0000

    Basic classes

--- src/Luggage.java
+++ src/Luggage.java
@@ -1,10 +1,10 @@
 
 public class Luggage {
 
-	private double accum_volume;
-	private double accum_weight;
-	private double accum_excessfees;
-	private int accum_numberofpassengers;
+	private double accum_volume = 0;
+	private double accum_weight = 0;
+	private double accum_excessfees = 0;
+	private int accum_numberofpassengers = 0;
 	
 	
 	
@@ -34,7 +34,7 @@ public class Luggage {
 	 * @param accum_volume
 	 */
 	public void setAccum_volume(double accum_volume) {
-		this.accum_volume = accum_volume;
+		this.accum_volume += accum_volume;
 	}
 	
 	
@@ -50,7 +50,7 @@ public class Luggage {
 	 * @param accum_weight
 	 */
 	public void setAccum_weight(double accum_weight) {
-		this.accum_weight = accum_weight;
+		this.accum_weight += accum_weight;
 	}
 	
 	
@@ -66,7 +66,7 @@ public class Luggage {
 	 * @param accum_excessfees
 	 */
 	public void setAccum_excessfees(double accum_excessfees) {
-		this.accum_excessfees = accum_excessfees;
+		this.accum_excessfees += accum_excessfees;
 	}
 	
 	
@@ -82,7 +82,7 @@ public class Luggage {
 	 * @param accum_numberofpassengers
 	 */
 	public void setAccum_numberofpassengers(int accum_numberofpassengers) {
-		this.accum_numberofpassengers = accum_numberofpassengers;
+		this.accum_numberofpassengers += accum_numberofpassengers;
 	}
 	
 }
