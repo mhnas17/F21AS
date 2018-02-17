440731390f97cb763250f199cc0a3a7963c47068
Wearside13 <ppa1@hw.ac.uk>
2018-02-17 15:35:49 +0000

    imported packages for read/write in manager class

--- src/core/Manager.java
+++ src/core/Manager.java
@@ -1,10 +1,12 @@
 package core;
-
+import java.util.InputMismatchException;
+import java.io.*;
 import Exceptions.InvalidBookingReference;
 import GUI.NewGui;
-import name_plane.Name;
 import name_plane.Plane;
-
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Manager {
  
@@ -18,5 +20,8 @@ public class Manager {
 		Booking b = new Booking("AF115",new Name("Minadakis George"),"A1320",false);
 		Flight c = new Flight("A13201","London","AEGEAN", new Plane (140,300,500));
 	}
+	
+
+	
 
 }
