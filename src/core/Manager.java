aee2fc41d7222a1c234e8701a335bbb54f091843
mhnas17 <giorgosminadakis@gmail.com>
2018-02-22 13:22:40 +0000

    comments

--- src/core/Manager.java
+++ src/core/Manager.java
@@ -15,6 +15,7 @@ public class Manager {
 	private BookingLists lists;
 	private NewGui gui;
 	private Luggage l;
+	
 	/** Reads the two csv files and creates the hashmaps and starts the Gui
 	 * @throws Exception
 	 */
@@ -34,7 +35,7 @@ public class Manager {
 			while (inputLine != null) {
 				// split line into parts
 				data = inputLine.split(",");
-				// create staff object
+				// define plane type
 				String planeType = data[3];
 				if (planeType.equals("A1")) {
 					p1 = new Plane(10, 200, 3.75);
@@ -43,10 +44,12 @@ public class Manager {
 				} else {
 					p1 = new Plane(13, 260, 4.875);
 				}
+				// create flight object
 				Flight s = new Flight(data[0], data[1], data[2], p1);
 				l = new Luggage(0, 0, 0, 0);
-				// add to list
+				// add to flights hashmap
 				entries.add(data[0], s);
+				// add to luggage hashmap
 				lug.add(data[0], l);
 				// read next line
 				inputLine = buff.readLine();
@@ -73,17 +76,18 @@ public class Manager {
 			String inputLine = buff.readLine(); // read first line
 			while (inputLine != null) {
 				// split line into parts
-				data = inputLine.split(",");
-				// create staff object
+				data = inputLine.split(",");				
 				Name n = new Name(data[1]);
+				// create booking object
 				Booking b = new Booking(data[0], n, data[2], Boolean.parseBoolean(data[3]));
-				// add to list
+				// add to booking hashmap
 				book.add(data[0] + n.getLastName(), b);
+				// add booking references to a list
 				lists.addBookingReferences(data[0]);
-				// lists.sysoutBooking();
+				// create name object for lastnames list
 				Name name = new Name(data[1]);
+				// add lastnames to a list
 				lists.addLastName(name.getLastName());
-				// lists.sysoutName();
 				// read next line
 				inputLine = buff.readLine();
 			}
@@ -107,6 +111,7 @@ public class Manager {
 	}
 
 	public void showGui() {
+		// create a gui object
 		gui = new NewGui(book, lug, lists, entries,l);
 	}
 
