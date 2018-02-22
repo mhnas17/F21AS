1e282206935c319d3acf05543022b0ba169dcc75
mhnas17 <giorgosminadakis@gmail.com>
2018-02-22 14:18:46 +0000

    more comments

--- src/GUI/NewGui.java
+++ src/GUI/NewGui.java
@@ -14,7 +14,7 @@ import core.Luggage;
 public class NewGui extends JFrame implements ActionListener {
 
 	/**
-	 * Creating the Gui and its components 
+	 * Creating the Gui and its components
 	 */
 	private static final long serialVersionUID = 1L;
 	ButtonGroup roleButtons = new ButtonGroup();
@@ -34,8 +34,8 @@ public class NewGui extends JFrame implements ActionListener {
 	private BookingLists lists;
 	private FlightMap fmap;
 	private Luggage lg;
-	 
-	public NewGui(BookingMap bmap, LuggageMap lmap, BookingLists blist, FlightMap fmap,Luggage lg) {
+
+	public NewGui(BookingMap bmap, LuggageMap lmap, BookingLists blist, FlightMap fmap, Luggage lg) {
 		this.book = bmap;
 		this.lugagges = lmap;
 		this.lists = blist;
@@ -75,7 +75,8 @@ public class NewGui extends JFrame implements ActionListener {
 		this.add(centerPanel, BorderLayout.CENTER);
 	}
 
-	/**The bottom panel of the Gui
+	/**
+	 * The bottom panel of the Gui
 	 * 
 	 */
 	private void setupSouthPanel() {
@@ -88,11 +89,12 @@ public class NewGui extends JFrame implements ActionListener {
 		southPanel.add(report);
 		southPanel.add(close);
 		close.addActionListener(this);
-		
+
 		this.add(southPanel, BorderLayout.SOUTH);
 	}
 
-	/** The top panel of the gui
+	/**
+	 * The top panel of the gui
 	 * 
 	 */
 	private void setupNorthPanel() {
@@ -112,28 +114,35 @@ public class NewGui extends JFrame implements ActionListener {
 
 	public void actionPerformed(ActionEvent event) {
 		if (event.getSource() == checkIn) {
-
+			// if someone checks in with luggage
 			if (luggage.isSelected() == true) {
 				try {
+					// checks if inputed last name and booking reference exist and if passenger has already checked in
 					if (lists.searchNames(lastName.getText()) == true && lists.searchBookings(bkngRef.getText()) == true
 							&& book.getValue(bkngRef.getText() + lastName.getText()).isCheckedin() == false) {
-
+						// accumulates volume of luggage for the flight
 						lugagges.getValue(book.getValue(bkngRef.getText() + lastName.getText()).getFlightcode())
 								.setAccum_volume(getVolume());
+						// accumulates weight of luggage for the flight
 						lugagges.getValue(book.getValue(bkngRef.getText() + lastName.getText()).getFlightcode())
 								.setAccum_weight(getWeight());
+						// accumulates excess fees of flight
 						lugagges.getValue(book.getValue(bkngRef.getText() + lastName.getText()).getFlightcode())
 								.setAccum_excessfees(getExcessfees());
+						// accumulates passengers of flight
 						lugagges.getValue(book.getValue(bkngRef.getText() + lastName.getText()).getFlightcode())
 								.setAccum_numberofpassengers(1);
+						// sets passenger as checked in
 						book.getValue(bkngRef.getText() + lastName.getText()).setCheckedin(true);
-						JOptionPane.showMessageDialog(this, "luggage Excess fees: £"+String.format("%.2f",lg.compExceessFees(getWeight())));
+						JOptionPane.showMessageDialog(this,
+								"luggage Excess fees: £" + String.format("%.2f", lg.compExceessFees(getWeight())));
 						JOptionPane.showMessageDialog(this, "Check in complete! have a pleasant flight!");
-						if(book.getCheckInReport(book)==true) {
+						// checks if everyone has checked in and if that is the case ends the program and generates report
+						if (book.getCheckInReport(book) == true) {
 							JOptionPane.showMessageDialog(this, "Everyone is checked in!");
 							lugagges.writeToFile("Report.txt", lugagges.getReport(fmap));
-				        	System.exit(1);
-							
+							System.exit(1);
+
 						}
 					} else if (!lists.searchNames(lastName.getText())) {
 						JOptionPane.showMessageDialog(this, "Last name doesn't exist!");
@@ -142,33 +151,32 @@ public class NewGui extends JFrame implements ActionListener {
 					} else if (book.getValue(bkngRef.getText() + lastName.getText()).isCheckedin() == true) {
 						JOptionPane.showMessageDialog(this, "You are already checked in!");
 					}
-				} 
-				catch (NumberFormatException e) {
+				} catch (NumberFormatException e) {
 					JOptionPane.showMessageDialog(this, "Please enter your bag dimension and weight");
-				}
-				catch (NegativeNumbers e) {
+				} catch (NegativeNumbers e) {
 					JOptionPane.showMessageDialog(this, "You have entered negative numbers,please try again");
-				}
-				catch (NullPointerException e) {
+				} catch (NullPointerException e) {
 					JOptionPane.showMessageDialog(this, "Incorrect name booking reference combination!");
 				}
 
-			} else {
+			} else { 		// case that passenger has no luggage
 				try {
+					// checks if inputed last name and booking reference exist and if passenger has already checked in
 					if (lists.searchNames(lastName.getText()) == true && lists.searchBookings(bkngRef.getText()) == true
 							&& book.getValue(bkngRef.getText() + lastName.getText()).isCheckedin() == false) {
-
+						// accumulates passengers of flight
 						lugagges.getValue(book.getValue(bkngRef.getText() + lastName.getText()).getFlightcode())
 								.setAccum_numberofpassengers(1);
+						// sets passenger as checked in
 						book.getValue(bkngRef.getText() + lastName.getText()).setCheckedin(true);
 						JOptionPane.showMessageDialog(this, "Check in complete! have a pleasant flight!");
-						if(book.getCheckInReport(book)==true) {
+						if (book.getCheckInReport(book) == true) {
 							JOptionPane.showMessageDialog(this, "Everyone is checked in!");
 							lugagges.writeToFile("Report.txt", lugagges.getReport(fmap));
-				        	System.exit(1);
-							
+							System.exit(1);
+
 						}
-					
+
 					} else if (!lists.searchNames(lastName.getText())) {
 						JOptionPane.showMessageDialog(this, "Last name doesn't exist!");
 					} else if (!lists.searchBookings(bkngRef.getText())) {
@@ -186,20 +194,20 @@ public class NewGui extends JFrame implements ActionListener {
 		if (event.getSource() == report) {
 			lugagges.writeToFile("Report.txt", lugagges.getReport(fmap));
 		}
-		
+
 		if (event.getSource() == close) {
-			String ObjButtons[] = {"Yes","Cancel"};
-	        int PromptResult = JOptionPane.showOptionDialog(null,"Are you sure you want to exit?","Exit Kiosk",JOptionPane.DEFAULT_OPTION,JOptionPane.WARNING_MESSAGE,null,ObjButtons,ObjButtons[1]);
-	        if(PromptResult==JOptionPane.YES_OPTION)
-	        {
-	        	//Writes the report on a file before closing in case the employee forgot to do so
-	        	lugagges.writeToFile("Report.txt", lugagges.getReport(fmap));
-	        	 System.exit(1);
-		    }
+			String ObjButtons[] = { "Yes", "Cancel" };
+			int PromptResult = JOptionPane.showOptionDialog(null, "Are you sure you want to exit?", "Exit Kiosk",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, ObjButtons, ObjButtons[1]);
+			if (PromptResult == JOptionPane.YES_OPTION) {
+				// Writes the report on a file before closing in case the employee forgot to do
+				// so
+				lugagges.writeToFile("Report.txt", lugagges.getReport(fmap));
+				System.exit(1);
+			}
 
 		}
-		
-		
+		// sets fields of luggage details editable or not according to luggage checkbox
 		if (event.getSource() == luggage) {
 			if (weight.isEditable() == false) {
 				weight.setEditable(true);
@@ -218,22 +226,26 @@ public class NewGui extends JFrame implements ActionListener {
 	}
 
 	public double getVolume() throws NumberFormatException {
-		if(length.getText()==null||width.getText()==null||height.getText()==null ) throw new NumberFormatException();
+		if (length.getText() == null || width.getText() == null || height.getText() == null)
+			throw new NumberFormatException();
 		// volume converted to m^3
-		double volume = lg.computeVolume(Double.parseDouble(length.getText()), Double.parseDouble(width.getText()),Double.parseDouble(height.getText()));
+		double volume = lg.computeVolume(Double.parseDouble(length.getText()), Double.parseDouble(width.getText()),
+				Double.parseDouble(height.getText()));
 		return volume;
 	}
 
 	public double getWeight() throws NumberFormatException {
-		if(weight.getText()==null ) throw new NumberFormatException();
+		if (weight.getText() == null)
+			throw new NumberFormatException();
 		double w = Double.parseDouble(weight.getText());
 		return w;
 	}
 
 	public double getExcessfees() throws NumberFormatException {
-		if(weight.getText()==null ) throw new NumberFormatException();
+		if (weight.getText() == null)
+			throw new NumberFormatException();
 		double fees = lg.compExceessFees(Double.parseDouble(weight.getText()));
 		return fees;
-		
+
 	}
 }
