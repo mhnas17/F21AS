4228f00a18fda83372f846f967ac128835c6256f
mhnas17 <giorgosminadakis@gmail.com>
2018-03-24 20:24:40 +0000

    last version of gui

--- src/GUI/Gui.java
+++ src/GUI/Gui.java
@@ -26,7 +26,6 @@ public class Gui extends JFrame implements Observer, ActionListener {
 	// GUI components
 	private JButton addButton = new JButton("Open Check-in Desk");
 	private JButton removeButton = new JButton("Close Check-in Desk");
-	private JCheckBox auto = new JCheckBox("Auto");
 
 	ArrayList<Thread> threads = new ArrayList<>();
 	Map<String, Thread> m = new ConcurrentHashMap<String, Thread>();
@@ -36,9 +35,8 @@ public class Gui extends JFrame implements Observer, ActionListener {
 	Container contentPane = new Container();
 
 	int x = 0;
-	
 
-	private JTextArea[] desks = new JTextArea[10];
+	private JTextArea[] desks = new JTextArea[4];
 	private JTextArea[] flights = new JTextArea[3];
 
 	JPanel centerPanel;
@@ -61,7 +59,6 @@ public class Gui extends JFrame implements Observer, ActionListener {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(100, 600);
 		setLocation(10, 20);
-		
 
 		// add button panel and result field to the content pane
 		Container contentPane = getContentPane();
@@ -75,14 +72,14 @@ public class Gui extends JFrame implements Observer, ActionListener {
 
 	private JPanel createNorthPanel() {
 		JPanel northPanel = new JPanel(new GridLayout(1, 3));
-		
-		for (int i = 0; i <3; i++) {
+
+		for (int i = 0; i < 3; i++) {
 			flights[i] = new JTextArea(5, 20);
 			flights[i].setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
 			flights[i].setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.LIGHT_GRAY));
 			northPanel.add(flights[i]);
 		}
-		
+
 		return northPanel;
 	}
 
@@ -98,9 +95,6 @@ public class Gui extends JFrame implements Observer, ActionListener {
 
 		southPanel.add(removeButton);
 		removeButton.addActionListener(this);
-		
-		southPanel.add(auto);
-		auto.addActionListener(this);
 
 		return southPanel;
 	}
@@ -135,34 +129,6 @@ public class Gui extends JFrame implements Observer, ActionListener {
 		}
 		return centerPanel;
 	}
-	
-	public void autoCheckIn() {
-		while(!wait.getTimerFinish()) {
-			System.out.println(desks.length);
-			if(wait.getQueueSize() +3 > desks.length) {
-				x++;
-
-				desks[x] = new JTextArea(5, 20);
-				desks[x].setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-				desks[x].setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.LIGHT_GRAY));
-
-				centerPanel.add(desks[x]);
-				centerPanel.revalidate();
-				centerPanel.repaint();
-
-				createCheckInDesk(x);
-			}
-			else {
-				removeCheckInDesk();
-
-				centerPanel.remove(desks[x]);
-				centerPanel.revalidate();
-				centerPanel.repaint();
-
-				x--;
-			}
-		}
-	}
 
 	public void actionPerformed(ActionEvent event) {
 		if (event.getSource() == addButton) {
@@ -188,14 +154,11 @@ public class Gui extends JFrame implements Observer, ActionListener {
 
 			x--;
 		}
-		if (auto.isSelected()) {
-			removeButton.setEnabled(false);
+
+		if (x == 3) {
 			addButton.setEnabled(false);
-			autoCheckIn();
-		}
-		if (!auto.isSelected()) {
-			removeButton.setEnabled(true);
-			addButton.setEnabled(true);
+		} else if (x == -1) {
+			removeButton.setEnabled(false);
 		}
 	}
 
@@ -216,16 +179,16 @@ public class Gui extends JFrame implements Observer, ActionListener {
 
 		waitingQueue.setText(args.toString());
 		String report = wait.checkInReport();
-		String flightReport1=p.getLuggageMap().FlightStatus(p.getFlightMap().getFlight("A1320"));
-		String flightReport2=p.getLuggageMap().FlightStatus(p.getFlightMap().getFlight("B2430"));
-		String flightReport3=p.getLuggageMap().FlightStatus(p.getFlightMap().getFlight("C3340"));
+		String flightReport1 = p.getLuggageMap().FlightStatus(p.getFlightMap().getFlight("A1320"));
+		String flightReport2 = p.getLuggageMap().FlightStatus(p.getFlightMap().getFlight("B2430"));
+		String flightReport3 = p.getLuggageMap().FlightStatus(p.getFlightMap().getFlight("C3340"));
 		flights[0].setText(flightReport1);
 		flights[1].setText(flightReport2);
 		flights[2].setText(flightReport3);
 		for (int i = 0; i <= x; i++) {
 			if (Thread.currentThread().getName().equals(Integer.toString(i))) {
-				int deskno = Integer.parseInt(Thread.currentThread().getName())+1;
-				desks[i].setText("Desk "+ deskno +": \n" + report);
+				int deskno = Integer.parseInt(Thread.currentThread().getName()) + 1;
+				desks[i].setText("Desk " + deskno + ": \n" + report);
 			}
 		}
 	}
